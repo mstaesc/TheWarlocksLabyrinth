22189078492a4e9b4ec4f44d8ba6777f0a1fe61f
taeschner-molly <116302858+taeschner-molly@users.noreply.github.com>
2022-12-15 22:56:44 -0800

    Added MainActivity base functionality as well as other fragments.

--- app/src/main/java/edu/uw/tcss450/mtaesc/thewarlockslabyrinth/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/mtaesc/thewarlockslabyrinth/MainActivity.java
@@ -2,13 +2,50 @@ package edu.uw.tcss450.mtaesc.thewarlockslabyrinth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "Inside onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "Inside onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "Inside onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "Inside onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "Inside onDestory");
+        super.onDestroy();
     }
 }
