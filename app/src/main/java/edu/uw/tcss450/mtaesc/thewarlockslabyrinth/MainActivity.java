6833f26f992b4f5fb7834079eb3cde76bd18d069
taeschner-molly <116302858+taeschner-molly@users.noreply.github.com>
2023-03-01 19:42:35 -0800

    changed theme, trying to hide action and status bar

--- app/src/main/java/edu/uw/tcss450/mtaesc/thewarlockslabyrinth/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/mtaesc/thewarlockslabyrinth/MainActivity.java
@@ -1,13 +1,14 @@
 package edu.uw.tcss450.mtaesc.thewarlockslabyrinth;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+
+//import edu.uw.tcss450.mtaesc.thewarlockslabyrinth.model.GameView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        View decorView = getWindow().getDecorView();
+//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+//        ActionBar actionBar = getActionBar();
+//        actionBar.hide();
+
+//        decorView.setOnSystemUiVisibilityChangeListener
+//                (new View.OnSystemUiVisibilityChangeListener() {
+//                    @Override
+//                    public void onSystemUiVisibilityChange(int visibility) {
+//                        // Note that system bars will only be "visible" if none of the
+//                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+//                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+//                            // TODO: The system bars are visible. Make any desired
+//                            // adjustments to your UI, such as showing the action bar or
+//                            // other navigational controls.
+//                        } else {
+//                            // TODO: The system bars are NOT visible. Make any desired
+//                            // adjustments to your UI, such as hiding the action bar or
+//                            // other navigational controls.
+//                        }
+//                    }
+//                });
+
         setContentView(R.layout.activity_main);
     }
 
@@ -29,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         Log.d(TAG, "Inside onResume");
         super.onResume();
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
     @Override
